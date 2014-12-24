506b4df8f35890536f04a9df9defe3fabd40d4b8
Rafael Minguet García <rminguet@gmail.com>
2014-12-24 11:55:24 +0100

    Added: edition of a post.

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express   = require('express'),
     router    = express.Router(),
     models    = require('../models'),
     sequelize = require('sequelize'),
+    async     = require('async'),
     utils     = require('../utils/utils');
 
 router.get('/add', function(req, res) {
@@ -38,16 +39,63 @@ router.get('/:alias/posts', utils.tools.checkAuth, function(req, res) {
 });
 
 router.get('/:alias/posts/edit', utils.tools.checkAuth, function(req, res) {
-  res.render('posts_edit');
+  var findPost = function(callback) {
+    models.Post.find(parseInt(req.param('id'))).success(function(post) {
+      return callback(null, post);
+    }).error(function() {
+      return callback(null, null);
+    });
+  };
+
+  var showPage = function(err, result) {
+    if (err) { throw err; }
+    res.render('posts_edit', { 'post': result[0] });
+  };
+
+  // 'async.parallel' makes possible that 'showPage' function only runs 
+  // after all functions array complete their work.
+  async.parallel([findPost], showPage);
 });
 
+/**
+*
+* Recently, 'findOrCreate' changed its behaviour and these changes are not
+* documented in Sequelized API yet. Now it takes two arguments instead of three:
+*   Model.findOrCreate({ where: where, defaults: defaults}, options);
+*   Reference: (https://github.com/sequelize/sequelize/wiki/Upgrading-to-2.0)
+*
+* Furthermore, it returns only one result (instead of two). The result returned
+* is an array with two values:
+*   - First value: the object found or created (if where clause didn't find
+*     anything).
+*   - Second Value: boolean -> true=created; false=found;
+*
+*/
 router.post('/:alias/posts/create', utils.tools.checkAuth, function(req, res) {
-  models.Post.create({
-    title: req.param('title'),
-    text: req.param('text'),
-    userId: req.session.user_id
-  }).then(function() {
-    res.redirect('/user/' + req.params.alias + '/posts');
+  var postIncoming = {
+        title: req.param('title'),
+        text: req.param('text'),
+        userId: req.session.user_id
+      },
+      where = (req.body.id === '') ? postIncoming : { 'id': parseInt(req.body.id)},
+      urlToRedirect = '/user/' + req.params.alias + '/posts';
+
+  models.Post.findOrCreate({
+    where: where,
+    defaults: {}
+  }).then(function(result) {
+    if (!result[1]) {
+      result[0].updateAttributes(postIncoming).success(function() {
+        res.redirect(urlToRedirect);
+      }).error(function(err) {
+        // This usually happens because an "ER_EMPTY_QUERY" error. This means
+        // our loved user didn't change anything when he sent the form.
+        console.log('ERROR!!----------->' + err.message);
+        res.redirect(urlToRedirect);
+      });
+    } else {
+      res.redirect(urlToRedirect);
+    }
   });
 });
 
